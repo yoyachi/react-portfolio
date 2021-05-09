e2e1c930148aac9a34bd7bc93ca8d66b05761697
Julissa Hash <69886471+yoyachi@users.noreply.github.com>
2021-05-09 07:19:35 -0500

    updated index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,8 +9,7 @@ function About() {
       <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
       <div className="my-2">
         <p>
-          
-          I am a full stack developer looking to transform and define current and new applications and websites creating a more effective and dynamic user interaction. I am currently taking a full stack developing bootcamp at the University of Texas gaining knowledge on newly developed skills in JavaScript, CSS, React.js and responsive web design, MERN.
+          I am a full stack developer instructed by the University of Texas with their  bootcamp coding program. This program helped me setting knowledge on newly development skills in JavaScript, CSS, React.js and responsive web design, MERN. With this knowledge I am looking to transform and define current and new applications, creating a more effective and dynamic user interaction. I am looking forward to join a developer company that would appreciate innovation, interactivity and desirable web design which is one of my biggest passions.  Altogether with this company, I would look to setup better solutions to a competitive and rapid changing developer world.
       </p>
       </div>
     </section>
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,66 +1,98 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
-import { validateEmail } from '../../utils/helpers';
+function Contact() {
+	const [formState, setFormState] = useState({
+		name: "",
+		email: "",
+		message: "",
+	});
 
-function ContactForm() {
-  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+	const [errorMessage, setErrorMessage] = useState("");
 
-  const [errorMessage, setErrorMessage] = useState('');
-  const { name, email, message } = formState;
+	const { name, email, message } = formState;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!errorMessage) {
-      console.log('Submit Form', formState);
-    }
-  };
+	function handleChange(e) {
+		if (e.target.name === "email") {
+			const isValid = validateEmail(e.target.value);
+			if (!isValid) {
+				setErrorMessage("Your email is invalid.");
+			} else {
+				if (!e.target.value.length) {
+					setErrorMessage(`${e.target.name} is required.`);
+				} else {
+					setErrorMessage("");
+				}
+			}
+		}
 
-  const handleChange = (e) => {
-    if (e.target.name === 'email') {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) {
-        setErrorMessage('Your email is invalid.');
-      } else {
-        setErrorMessage('');
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`${e.target.name} is required.`);
-      } else {
-        setErrorMessage('');
-      }
-    }
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log('Handle Form', formState);
-    }
-  };
+		if (!errorMessage) {
+			setFormState({ ...formState, [e.target.name]: e.target.value });
+		}
+	}
 
-  return (
-    <section>
-      <h1 data-testid="h1tag">Contact me</h1>
-      <form id="contact-form" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
-          </div>
-        )}
-        <button data-testid="button" type="submit">Submit</button>
-      </form>
-    </section>
-  );
+	function handleBlank(e) {
+		if (e.target.name === "Name" || e.target.name === "Message") {
+			if (!e.target.value.length) {
+				setErrorMessage(`${e.target.name} is required.`);
+			} else {
+				setErrorMessage("");
+			}
+		}
+
+		if (!errorMessage) {
+			setFormState({ ...formState, [e.target.name]: e.target.value });
+		}
+	}
+
+	return (
+		<section>
+			<div className="center">
+				<h2 className="page-header">Contact Me</h2>
+			</div>
+			<div>
+				<form id="contact-form">
+					<div>
+						<label htmlFor="Name">Name:</label>
+						<br></br>
+						<input
+							type="text"
+							defaultValue={name}
+							onBlur={handleBlank}
+							name="Name"
+						/>
+					</div>
+					<div>
+						<label htmlFor="email">Email address:</label>
+						<br></br>
+						<input
+							type="email"
+							defaultValue={email}
+							name="email"
+							onBlur={handleChange}
+						/>
+					</div>
+					<div>
+						<label htmlFor="Message">Message:</label>
+						<br></br>
+						<textarea
+							name="Message"
+							defaultValue={message}
+							onBlur={handleBlank}
+							rows="5"
+						/>
+					</div>
+					{errorMessage && (
+						<div>
+							<p className="error-text">{errorMessage}</p>
+						</div>
+					)}
+					<button type="submit">Submit</button>
+				</form>
+			</div>
+		</section>
+	);
 }
 
-export default ContactForm;
+export default Contact;
+
